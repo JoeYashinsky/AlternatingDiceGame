1e856b05900352031b42403b6dbbb4909104745c
Your Name <joey.yashinsky@gmail.com>
2021-03-01 18:16:08 -0500

    functionality for rolling of the dice started

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generating a random dice roll
+
+    // 2. Display the dice
+
+    // 3. Check if a 1 was rolled
+})
