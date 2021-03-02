a36e125955a4fde51fcc0baffeaec1d8f35a5196
Your Name <joey.yashinsky@gmail.com>
2021-03-02 00:43:37 -0500

    'current' changed to 'score--' which is the correct element name

--- script.js
+++ script.js
@@ -23,6 +23,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
     // 1. Generating a random dice roll
@@ -39,10 +47,18 @@ btnRoll.addEventListener('click', function () {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
     }
+});
+
+btnHold.addEventListener('click', function () {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // 2. Check if player' score has reached 100 (goal)
+    //  End the game
+
+    // 3. Switch to the other player
+    switchPlayer();
 })
