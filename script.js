c39a3b20ea79c9989f03eaf032b8cff6b1a1544d
Your Name <joey.yashinsky@gmail.com>
2021-03-02 01:10:22 -0500

    game play should now end when a player reaches goal score and no more dice rolls allowed (until game is reset)

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -22,17 +21,19 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let stillPlaying = true;
 
 const switchPlayer = function () {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-}
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
+  if (stillPlaying) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -42,27 +43,38 @@ btnRoll.addEventListener('click', function () {
 
     // 3. Check if a 1 was rolled.
     if (dice !== 1) {
-        // Add dice to the current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+      // Add dice to the current score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
     } else {
-        // Switch to next player
-        switchPlayer();
+      // Switch to next player
+      switchPlayer();
     }
+  }
 });
 
 btnHold.addEventListener('click', function () {
+  if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
     // 2. Check if player' score has reached 100 (goal)
-    if (scores[activePlayer] >= 10) {
-        // End the game
-        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    if (scores[activePlayer] >= 50) {
+      // End the game
+      stillPlaying = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
     } else {
-        // 3. Switch to the other player
-        switchPlayer();
+      // 3. Switch to the other player
+      switchPlayer();
     }
+  }
 });
