b5d9a0ecb9e06263625769ad422566edaa1ecd9c
Your Name <joey.yashinsky@gmail.com>
2021-03-01 19:04:53 -0500

    current score for a player should be cumulative based on new roll being added to existing point total

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -14,6 +17,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
     // 1. Generating a random dice roll
@@ -24,5 +29,12 @@ btnRoll.addEventListener('click', function () {
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check if a 1 was rolled.
-    //  If true, switch to other player.
+    if (dice !== 1) {
+        // Add dice to the current score
+        currentScore += dice;
+        current0El.textContent = currentScore; // CHANGE LATER
+
+    } else {
+        // Switch to next player
+    }
 })
